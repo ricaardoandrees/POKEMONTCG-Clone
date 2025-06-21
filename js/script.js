95ff586ec4a616ad68c0fa7bd061316255e16f16
Leozama <lglezama.23@est.ucab.edu.ve>
2025-06-21 19:37:30 -0400

    carpetas y vista de index

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const dialog = document.getElementById("card-dialog");
+const openDialog = document.getElementById("openDialog");
+
+openDialog.addEventListener("click", () => {
+	dialog.showModal();
+});
